a265a196307d222887a86c375fb60d46740d21f2
Pepijn Looije <p@epij.nl>
2021-03-06 14:08:00 -0300

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject nl.epij/google-cloud-function-ring-adapter "0.1.0-alpha2"
+(defproject nl.epij/google-cloud-function-ring-adapter "0.1.0-alpha3"
   :description "A (Clojure) ring adapter for the Google Cloud Function Java Runtime on Google Cloud Platform"
   :url "https://github.com/pepijn/google-cloud-function-ring-adapter"
   :license {:name "MIT"
