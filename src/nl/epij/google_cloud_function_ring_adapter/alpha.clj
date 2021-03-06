0136adccd3106f78ccaf465328e0b364d4000249
Pepijn Looije <p@epij.nl>
2021-03-06 14:06:34 -0300

    Make more ring compliant
    Downcase headers, join string values in headers

--- src/nl/epij/google_cloud_function_ring_adapter/alpha.clj
+++ src/nl/epij/google_cloud_function_ring_adapter/alpha.clj
@@ -1,20 +1,30 @@
 (ns nl.epij.google-cloud-function-ring-adapter.alpha
   (:require [clojure.string :as str])
   (:import (com.google.cloud.functions HttpRequest HttpResponse)
-           (java.io BufferedWriter)))
+           (java.io BufferedWriter)
+           [java.util Optional]))
+
+(defn process-response!
+  [{:keys [status headers body]} http-response]
+  (doseq [[key value] headers]
+    (.appendHeader http-response key value))
+  (.setStatusCode http-response status)
+  (when body (.write ^BufferedWriter (.getWriter http-response) body)))
 
 (defn adapter
   [^HttpRequest http-request ^HttpResponse http-response handler]
-  (let [{:keys [status headers body]}
-        (handler {:request-method (-> (.getMethod http-request)
+  (prn)
+  (let [headers       (into {}
+                            (map (fn [[k v]] [(str/lower-case k) (str/join v)]))
+                            (.getHeaders http-request))
+        query-string  ^Optional (.getQuery http-request)
+        query-string' (.orElse query-string nil)]
+    (-> (handler {:request-method (-> (.getMethod http-request)
                                       str/lower-case
                                       keyword)
                   :uri            (.getUri http-request)
                   :path           (.getPath http-request)
-                  :query-string   (.getQuery http-request)
-                  :headers        (.getHeaders http-request)
-                  :body           (.getInputStream http-request)})]
-    (doseq [[key value] headers]
-      (.appendHeader http-response key value))
-    (.setStatusCode http-response status)
-    (when body (.write ^BufferedWriter (.getWriter http-response) body))))
+                  :query-string   query-string'
+                  :headers        headers
+                  :body           (.getInputStream http-request)})
+        (process-response! http-response))))
