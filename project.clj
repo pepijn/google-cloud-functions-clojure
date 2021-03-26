cbfa0ca728ca3e6a509ddee4b8cdee60792ab4e0
Pepijn Looije <p@epij.nl>
2021-03-26 15:53:46 -0300

    Include the structed JSON logging logback.xml in the JAR

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject nl.epij/google-cloud-function-ring-adapter "0.1.0-alpha6"
+(defproject nl.epij/google-cloud-function-ring-adapter "0.1.0-alpha7"
   :description "A (Clojure) ring adapter for the Google Cloud Function Java Runtime on Google Cloud Platform"
   :url "https://github.com/pepijn/google-cloud-function-ring-adapter"
   :license {:name "MIT"
             :url  "https://opensource.org/licenses/MIT"}
-  :source-paths ["src/main/clojure"]
-  :java-source-paths ["src/main/java"]
+  :source-paths ["src/clojure"]
+  :java-source-paths ["src/java" "src/resources"]
   :javac-options ["-target" "11" "-source" "11"]
   :dependencies [[org.clojure/clojure "1.10.2"]
                  [com.google.cloud.functions/functions-framework-api "1.0.1"]
