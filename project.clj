bb303fb8b68a16ebe0cc07504ca117fff31156e8
Pepijn Looije <p@epij.nl>
2021-04-02 15:29:26 -0300

    [GitHub Actions] Don't download dependencies on a cache hit

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject nl.epij/google-cloud-function-ring-adapter "0.1.0-SNAPSHOT"
-  :description "A (Clojure) ring adapter for the Google Cloud Function Java Runtime on Google Cloud Platform"
+  :description "A (Clojure) ring adapter for the Cloud Function Java Runtime on Google Cloud Platform"
   :url "https://github.com/pepijn/google-cloud-function-ring-adapter"
   :license {:name "MIT"
             :url  "https://opensource.org/licenses/MIT"}
