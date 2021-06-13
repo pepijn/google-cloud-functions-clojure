3e5f7bfb780a3763dc22ca322689ce951c514488
Pepijn Looije <p@epij.nl>
2021-06-12 23:41:17 -0300

    Make example and deploy tests use repo library

--- deploy/bin/test.sh
+++ deploy/bin/test.sh
@@ -4,4 +4,4 @@ set -euxo pipefail
 
 cd "$(dirname "$(realpath "$0")")/.."
 
-clojure -M:test
+clojure -Sdeps '{:replace-deps {nl.epij/google-cloud-functions-ring-adapter {:local/root ".."}}}' -M:test
